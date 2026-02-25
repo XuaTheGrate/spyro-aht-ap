ca84a2fcbb7e19524d34854f6649352bab1f0d85
Maya <xu4.yr4171@gmail.com>
2026-02-25 20:00:30 +1300

    add receiveitem handler and basic ram storage

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import os.path
 import ssl
+from collections import Counter
 from dataclasses import dataclass
 from typing import Any, Literal
 
@@ -55,6 +56,14 @@ class NetworkPlayer:
 
 
 @dataclass
+class NetworkItem:
+    item: int
+    location: int
+    player: int
+    flags: int
+
+
+@dataclass
 class WebsocketConnected:
     cmd: Literal["Connected"]
     team: int
@@ -94,6 +103,9 @@ class APClient:
         
         self._pine_task: asyncio.Task | None = None
         self._ap_task: asyncio.Task | None = None
+
+        # TODO: not the ideal way to re-sync items i think, but that is for future me to fix
+        self.synced_items = Counter()
     
     def _connect_payload(self) -> str:
         return encode([{
@@ -119,6 +131,7 @@ class APClient:
         _roominfo = await self._socket.recv()
         print(f"Connecting to slot '{self.slot}'")
         await self._socket.send(self._connect_payload())
+        #await self._socket.send("""[{"cmd":"GetDataPackage","games":["Spyro: A Hero's Tail"]}]""")
 
         self.set_tasks()
         
@@ -132,6 +145,9 @@ class APClient:
         self._pine_task = asyncio.create_task(self._pine_loop(), name="Pine Loop")
     
     async def _send_locations(self):
+        if len(self.checked_locations) == 0: return
+
+        print(f"Sending {len(self.checked_locations)} locations")
         payload = encode([{
             "cmd": "LocationChecks",
             "locations": list(self.checked_locations)
@@ -143,6 +159,25 @@ class APClient:
         await self._send_locations()
         print(f"Synced {len(self.checked_locations)} locations")
         self.ready.set()
+    
+    async def _received_item(self, item: NetworkItem) -> None:
+        # TODO: add prompt to in game via Trina
+        print(f"Received item {item.item:x}")
+        match item.item:
+            case 0x1: # Double Jump
+                await self.set_double_jump(True)
+            case 0x8: # Dark Gem
+                count = await self.get_item_count(consts.DARK_GEM_COUNT)
+                if count < self.synced_items[0x8]:
+                    await self.add_dark_gem()
+            case 0x9: # Light Gem
+                count = await self.get_item_count(consts.LIGHT_GEM_COUNT)
+                if count < self.synced_items[0x9]:
+                    await self.add_light_gem()
+            case 0xA: # Dragon Egg
+                count = await self.get_item_count(consts.DRAGON_EGG_COUNT)
+                if count < self.synced_items[0xA]:
+                    await self.add_dragon_egg()
 
     async def _process_packet(self, payload: dict[str, Any]) -> None:
         match payload['cmd']:
@@ -151,20 +186,28 @@ class APClient:
                     k.pop('class')
                 payload['players'] = [NetworkPlayer(**k) for k in payload['players']]
                 await self._websocket_connected_packet(WebsocketConnected(**payload))
+            case "PrintJSON":
+                print(f"Archipelago: {payload['data']}")
+            case "ReceivedItems":
+                for i in payload['items']:
+                    self.synced_items[i['item']] += 1
+                    i.pop("class")
+                    ni = NetworkItem(**i)
+                    await self._received_item(ni)
             case _ as c:
-                print(f"WARNING: Unhandled packet command '{c}'")
+                print(f"WARNING: Unhandled packet command '{c}': {payload}")
 
     async def _ap_loop(self):
         while not self.exit.is_set():
             packets = decode(await self._socket.recv()) # type:ignore
             for packet in packets:
                 await self._process_packet(packet)
-        await self._socket.close()
+        await asyncio.wait_for(self._socket.close(), 3.0)
     
     async def _pine_loop(self):
         await self.ready.wait()
         while not self.exit.is_set():
-            for loc, addr, orv in consts.LOCATIONS:
+            for loc, item_type, addr, orv in consts.LOCATIONS:
                 if loc in self.checked_locations:
                     continue
 
@@ -172,6 +215,7 @@ class APClient:
                 if data | orv == data:
                     print(f"Dispatching item 0x{loc:x} ({addr:x})")
                     self.checked_locations.add(loc)
+                    await self.remove_item(item_type)
                     await self._send_locations()
             
             await asyncio.sleep(1)
@@ -180,6 +224,45 @@ class APClient:
     async def close(self):
         self.exit.set()
         await asyncio.wait([self._pine_task, self._ap_task]) # type: ignore
+    
+    async def set_double_jump(self, to: bool):
+        flags = consts.PlayerFlags(await self.pine.read_int32(consts.PLAYER_FLAGS))
+        if to:
+            await self.pine.write_int32(consts.PLAYER_FLAGS, flags | consts.PlayerFlags.DoubleJump)
+        else:
+            await self.pine.write_int32(consts.PLAYER_FLAGS, flags & ~consts.PlayerFlags.DoubleJump)
+    
+    async def remove_item(self, item: int):
+        print(f"Early removing {item:x}")
+        match item:
+            case 0x1: # Double Jump
+                if not await self.should_have_item(consts.ITEM_STORAGE_DOUBLE_JUMP):
+                    await self.set_double_jump(False)
+            case 0x8: # Dark Gem
+                await self.add_dark_gem(-1)
+            case 0x9: # Light Gem
+                await self.add_light_gem(-1)
+            case 0xA: # Dragon Egg
+                await self.add_dragon_egg(-1)
+    
+    async def should_have_item(self, key: tuple[int, int]) -> bool:
+        flag = await self.pine.read_int8(consts.ITEM_STORAGE + key[0])
+        return flag & key[1] == key[1]
+
+    async def get_item_count(self, addr: int):
+        return await self.pine.read_int8(addr)
+    
+    async def add_dark_gem(self, amount: int = 1):
+        count = await self.get_item_count(consts.DARK_GEM_COUNT)
+        await self.pine.write_int8(consts.DARK_GEM_COUNT, count + amount)
+    
+    async def add_light_gem(self, amount: int = 1):
+        count = await self.get_item_count(consts.LIGHT_GEM_COUNT)
+        await self.pine.write_int8(consts.LIGHT_GEM_COUNT, count + amount)
+    
+    async def add_dragon_egg(self, amount: int = 1):
+        count = await self.get_item_count(consts.DRAGON_EGG_COUNT)
+        await self.pine.write_int8(consts.DRAGON_EGG_COUNT, count + amount)
 
 
 async def main():
--- consts.py
+++ consts.py
@@ -1,8 +1,42 @@
+from enum import IntFlag
+
 MD5 = '4a8320b24738334127b29de343c534ae'
 IDN = 'SLUS_208.84'
 
-# unsure if it will be useful, but AHT has plenty of spare ram i can use as storage. this address provides a good starting point and leaves approx. 12kb
 ITEM_STORAGE = 0x503280
+
+# Items are stored at 0x503280
+# the first tuple item is an offset from this address in multiples of 2
+# the second item is the bit flag (0x1 - 0x80)
+ITEM_STORAGE_DOUBLE_JUMP = (0x0, 0x1)
+
+DARK_GEM_COUNT = 0x502057
+LIGHT_GEM_COUNT = 0x502056
+DRAGON_EGG_COUNT = 0x502058
+
+PLAYER_FLAGS = 0x502028
+
+class PlayerFlags(IntFlag):
+    DoubleJump = 0x1
+    UNUSED_1 = 0x2
+    SparxHealthUpgrade = 0x4
+    UNDETERMINED_1 = 0x8
+    PoleSpin = 0x10
+    IceBreath = 0x20
+    LightningBreath = 0x40
+    WaterBreath = 0x80
+    UNUSED_2 = 0x100
+    DoubleGems = 0x200
+    UNUSED_3 = 0x400
+    UNUSED_4 = 0x800
+    SuperchargeGadget = 0x1000
+    InvincibilityGadget = 0x2000
+    LOCKPICK_FLAG = 0x4000
+    WingShield = 0x8000
+    WallKick = 0x10000
+    Shockwave = 0x20000
+    ButterflyJar = 0x40000
+    UNUSED_5 = 0x80000
 
 NOP_ADDR = [
 	(0x96de0, b'\x01\x00\x42\x34', b'\x00\x00\x00\x00') # disable double jump from elder tomas
@@ -10,26 +44,26 @@ NOP_ADDR = [
 
 LOCATIONS = {
 	# Dragon Village
-	(0x0, 0x5021b5, 0x80), # Double Jump from Elder Tomas
-	(0x1, 0x502775, 0x2),  # Dark Gem next to Elder Tomas
-	(0x2, 0x502774, 0x80), # Dark Gem next to Ember
-	(0x3, 0x502782, 0x2),  # Dragon Egg past Hunter
-	(0x4, 0x5027c6, 0x80), # Light Gem inside Nursery
+	(0x1, 0x1, 0x5021b5, 0x80), # Double Jump from Elder Tomas
+	(0x2, 0x8, 0x502775, 0x2),  # Dark Gem next to Elder Tomas
+	(0x3, 0x8, 0x502774, 0x80), # Dark Gem next to Ember
+	(0x4, 0xA, 0x502782, 0x2),  # Dragon Egg past Hunter
+	(0x5, 0x9, 0x5027c6, 0x80), # Light Gem inside Nursery
     # TODO: make this check more strict
-	(0x5, 0x5027c9, 0x18), # Dragon Egg in Locked Chest before Ball Gadget
+	(0x6, 0xA, 0x5027c9, 0x18), # Dragon Egg in Locked Chest before Ball Gadget
     # TODO: make this check more strict
-	(0x6, 0x5027cf, 0x6),  # Light Gem in Locked Chest after Double Jump Cliff
-	(0x7, 0x502792, 0x8),  # Dragon Egg behind Breakable Wall near Sgt. Byrd
-	(0x8, 0x502791, 0x40), # Light Gem above Professor Mole
-	(0x9, 0x502777, 0x4),  # Light Gem after Platforming near Sgt. Byrd
-	(0xA, 0x502775, 0x40), # Dark Gem after Platforming near Sgt. Byrd
-	(0xB, 0x502776, 0x40), # Dragon Egg after Dark Gem near Sgt. Byrd
+	(0x7, 0x9, 0x5027cf, 0x6),  # Light Gem in Locked Chest after Double Jump Cliff
+	(0x8, 0xA, 0x502792, 0x8),  # Dragon Egg behind Breakable Wall near Sgt. Byrd
+	(0x9, 0x9, 0x502791, 0x80), # Light Gem above Professor Mole
+	(0xA, 0x9, 0x502777, 0x4),  # Light Gem after Platforming near Sgt. Byrd
+	(0xB, 0x8, 0x502775, 0x40), # Dark Gem after Platforming near Sgt. Byrd
+	(0xC, 0xA, 0x502776, 0x40), # Dragon Egg after Dark Gem near Sgt. Byrd
     # TODO: resolve mini games
-	# (0xC, 0x000000, 0x0),  # Dragon Egg from Sgt. Byrd
-	# (0xD, 0x000000, 0x0),  # Light Gem from Sgt. Byrd
+	# (0xD, 0xA, 0x000000, 0x0),  # Dragon Egg from Sgt. Byrd
+	# (0xE, 0x9, 0x000000, 0x0),  # Light Gem from Sgt. Byrd
     # TODO: make this check more strict
-	(0xE, 0x5027D0, 0x6),  # Light Gem in Locked Chest before Swamp
+	(0xF, 0x9, 0x5027D0, 0x6),  # Light Gem in Locked Chest before Swamp
     # TODO
-    # (0xF, 0x000000, 0x0),  # Dragon Egg in Gnasty's Lair after Flame
-    # (0x10, 0x00000, 0x0),  # Lightning Breath from Gnasty Gnorc
+    # (0x10, 0xA, 0x000000, 0x0),  # Dragon Egg in Gnasty's Lair after Flame
+    # (0x11, 0x5, 0x00000, 0x0),  # Lightning Breath from Gnasty Gnorc
 }
